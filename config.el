e5227010fbc8c03179f9e89206475390dddf19a2
ananas <lucienfiorini@protonmail.com>
2020-08-01 16:26:22 +0200

    Polish structure

--- config.el
+++ config.el
@@ -1,16 +1,16 @@
-;; Name and Email
 (setq user-full-name "Lucien Fiorini"
       user-mail-address "lucienfiorini@protonmail.com")
 
-;; Font
-(setq doom-font (font-spec :family "Source Code Pro" :size 15))
+(setq doom-font (font-spec :family "Source Code Pro"))
 
-;; Theme
 (setq doom-theme 'doom-gruvbox)
 
 (setq display-line-numbers-type 'relative)
 
 (setq org-directory "~/misc/org/")
+
+(eval-after-load "org"
+  '(require 'ox-twbs nil t))
 
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
@@ -20,47 +20,4 @@
 
 (add-hook 'c++-mode-hook 'my-c++-mode-hook)
 
-(eval-after-load "org"
-  '(require 'ox-twbs nil t))
-
 (require 'gdscript-mode)
-
-(custom-set-variables
- '(ansi-color-names-vector
-   ["#272C36" "#BF616A" "#A3BE8C" "#EBCB8B" "#81A1C1" "#B48EAD" "#88C0D0" "#ECEFF4"])
- '(custom-safe-themes
-   (quote
-    ("e2acbf379aa541e07373395b977a99c878c30f20c3761aac23e9223345526bcc" default)))
- '(fci-rule-color "#4C566A")
- '(jdee-db-active-breakpoint-face-colors (cons "#191C25" "#81A1C1"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#191C25" "#A3BE8C"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#191C25" "#434C5E"))
- '(objed-cursor-color "#BF616A")
- '(pdf-view-midnight-colors (cons "#ECEFF4" "#2E3440"))
- '(rustic-ansi-faces
-   ["#2E3440" "#BF616A" "#A3BE8C" "#EBCB8B" "#81A1C1" "#B48EAD" "#88C0D0" "#ECEFF4"])
- '(vc-annotate-background "#2E3440")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#A3BE8C")
-    (cons 40 "#bbc28b")
-    (cons 60 "#d3c68b")
-    (cons 80 "#EBCB8B")
-    (cons 100 "#e2b482")
-    (cons 120 "#d99d79")
-    (cons 140 "#D08770")
-    (cons 160 "#c68984")
-    (cons 180 "#bd8b98")
-    (cons 200 "#B48EAD")
-    (cons 220 "#b77f96")
-    (cons 240 "#bb7080")
-    (cons 260 "#BF616A")
-    (cons 280 "#a05b67")
-    (cons 300 "#815664")
-    (cons 320 "#625161")
-    (cons 340 "#4C566A")
-    (cons 360 "#4C566A")))
- '(vc-annotate-very-old-color nil))
-
-(custom-set-faces
- )
